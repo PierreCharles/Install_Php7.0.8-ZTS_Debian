6801858fe73e4348c567c1d303ba596a5f58221e
root <root@vm-jessie-php7>
2016-07-21 12:35:17 +0200

    Refactoring file procject

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Install_Php7.0.8-ZTS_Debian8
+
+##Install
+
+git clone https://github.com/PierreCharles/Install_Php7.0.8-ZTS_Debian8.git
+
+./install.sh
+
+#Information
+
+The execution time of the script is approximately 30 minutes to 1 hour
+
+Configuration :
+
+	--prefix=/usr/local/php7 \
+        --with-config-file-scan-dir=/usr/local/php7/etc/conf.d \
+        --enable-bcmath \
+        --enable-calendar \
+        --enable-exif \
+        --enable-dba \
+        --enable-ftp \
+        --with-gettext \
+        --with-gd \
+        --with-jpeg-dir \
+        --enable-mbstring \
+        --with-mcrypt \
+        --with-mhash \
+        --enable-opcache \
+        --enable-mysqlnd \
+        --with-mysql=mysqlnd \
+        --with-mysqli=mysqlnd \
+        --with-pdo-mysql=mysqlnd \
+        --with-mysql-sock=/var/run/mysqld/mysqld.sock \
+        --with-openssl \
+        --enable-pcntl \
+        --enable-soap \
+        --enable-sockets \
+        --enable-sysvmsg \
+        --enable-sysvsem \
+        --enable-sysvshm \
+        --with-zlib \
+        --enable-zip \
+        --with-readline \
+        --with-curl \
+        --enable-shmop \
+        --enable-fpm \
+        --enable-maintainer-zts \
+        --enable-pthreads=shared \
+        --with-tsrm-pthreads \
+        --enable-fastcgi \
+        --with-fpm-user=www-data \
+        --with-fpm-group=www-data"
--- install.sh
+++ install.sh
@@ -1,80 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# Dependencies
-apt-get update
-apt-get install -y \
-    build-essential \
-    pkg-config \
-    git-core \
-    autoconf \
-    bison \
-    libxml2-dev \
-    libbz2-dev \
-    libmcrypt-dev \
-    libicu-dev \
-    libssl-dev \
-    libcurl4-openssl-dev \
-    libltdl-dev \
-    libjpeg-dev \
-    libpng-dev \
-    libpspell-dev \
-    libreadline-dev
-
-mkdir -p /usr/local/php7
-git clone https://github.com/php/php-src.git
-cd php-src
-git checkout PHP-7.0.8
-git pull
-
-./buildconf --force
-
-CONFIGURE_STRING="--prefix=/usr/local/php7 \
-                  --with-config-file-scan-dir=/usr/local/php7/etc/conf.d \
-		  --enable-bcmath \
-                  --enable-calendar \
-                  --enable-exif \
-                  --enable-dba \
-                  --enable-ftp \
-                  --with-gettext \
-                  --with-gd \
-                  --with-jpeg-dir \
-                  --enable-mbstring \
-                  --with-mcrypt \
-                  --with-mhash \
-                  --enable-opcache \
-		  --enable-mysqlnd \
-                  --with-mysql=mysqlnd \
-                  --with-mysqli=mysqlnd \
-                  --with-pdo-mysql=mysqlnd \
-                  --with-mysql-sock=/var/run/mysqld/mysqld.sock \
-		  --with-openssl \
-                  --enable-pcntl \
-                  --enable-soap \
-                  --enable-sockets \
-                  --enable-sysvmsg \
-                  --enable-sysvsem \
-                  --enable-sysvshm \
-                  --with-zlib \
-                  --enable-zip \
-                  --with-readline \
-                  --with-curl \
-		  --enable-shmop \
-		  --enable-fpm \
-                  --enable-maintainer-zts \
-    		  --enable-pthreads=shared \
-    		  --with-tsrm-pthreads \
-		  --enable-fastcgi \
- 		  --with-fpm-user=www-data \
-                  --with-fpm-group=www-data"
-
-./configure $CONFIGURE_STRING
-
-#Make
-make
-make test
-make install
-
 # Create a dir for storing PHP module conf
 mkdir -p /usr/local/php7/etc/conf.d
 
@@ -92,3 +18,4 @@ cp conf/php7-fpm.init /etc/init.d/php7-fpm
 chmod +x /etc/init.d/php7-fpm
 update-rc.d php7-fpm defaults
 service php7-fpm start
+
